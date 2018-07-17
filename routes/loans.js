1808603ccf700f248da2c9c8b40e0c789920f524
Randy's Mac <randydavidl78@gmail.com>
2018-07-17 19:37:16 -0400

    Add new loan view

--- routes/loans.js
+++ routes/loans.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('loans/index', { title: 'Loans' })
 });
 
+/* Create a new loan */
+router.get('/add', function(req, res, next) {
+  res.render('loans/loan_add', { title: 'New Loan' })
+});
+
 module.exports = router;
