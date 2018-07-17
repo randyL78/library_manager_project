29393077b57346f801970c0d1dffd7515e146772
Randy's Mac <randydavidl78@gmail.com>
2018-07-17 18:19:49 -0400

    Add book detail view

--- routes/books.js
+++ routes/books.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET all books listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('books/index', { title: 'Books' })
+});
+
+/* Get the deatails of a single book */
+router.get('/book', (req, res) => {
+  res.render('books/book_detail', {title: "A Brief History of Time"})
 });
 
 module.exports = router;
