1931fa4ae3bfaff44a9e57f7f230c3dcc5766ffc
Randy's Mac <randydavidl78@gmail.com>
2018-07-17 16:47:47 -0400

    Add all loans view

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 const booksRouter = require('./routes/books');
 const patronsRouter = require('./routes/patrons');
+const loansRouter = require('./routes/loans');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/books', booksRouter);
 app.use('/patrons', patronsRouter)
+app.use('/loans', loansRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
