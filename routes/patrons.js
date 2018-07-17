d3e518689ff5059c8977410d07b2149ef48aa74a
Randy's Mac <randydavidl78@gmail.com>
2018-07-17 19:12:16 -0400

    Add new patron and patron detail views

--- routes/patrons.js
+++ routes/patrons.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('patrons/index', { title: 'Patrons' })
 });
 
+/* GET details of one patron. */
+router.get('/patron', function(req, res, next) {
+  res.render('patrons/patron_detail', { title: 'Andrew Chalkley' })
+});
+
+/* Add a new Patron. */
+router.get('/add', function(req, res, next) {
+  res.render('patrons/patron_add', { title: 'New Patron' })
+});
+
 module.exports = router;
